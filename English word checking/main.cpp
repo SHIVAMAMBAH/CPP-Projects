865d74b63f600b01a6c06f0aa4df33e89350e88d
SDiHALF <shivamsharma22468@gmail.com>
2024-11-10 00:54:00 +0530

    Update main.cpp

--- English word checking/main.cpp
+++ English word checking/main.cpp
@@ -1,1 +1,42 @@
 
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+
+int main()
+{
+	std::ifstream file("words_alpha.txt");
+	if (!file)
+	{
+		std::cerr << "Error Opening File";
+		return 1;
+	}
+
+	std::vector<std::string> words;
+	std::string word;
+
+	while (file >> word)
+	{
+		words.push_back(word);
+	}
+
+	file.close();
+
+	std::sort(words.begin(), words.end()); // Ensure sorted order
+
+	std::string targetWord;
+	std::cout << "Enter a word: ";
+	std::cin >> targetWord;
+
+	if (std::binary_search(words.begin(), words.end(), targetWord))
+	{
+		std::cout << targetWord << " found";
+	}
+	else
+	{
+		std::cout << targetWord << " not found";
+	}
+
+	return 0;
+}
